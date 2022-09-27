a5d4c22bf975d38d668803972d739889d58b69ba
putrifahriani29 <114386462+putrifahriani29@users.noreply.github.com>
2022-09-27 21:25:39 +0700

    Add files via upload

---
+++ TugasPBOjava.java
@@ -0,0 +1,14 @@
+
+
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
+public class TugasPBO {
+     public static void main(String[] args) {
+        String input;
+        input = JOptionPane.showInputDialog("Anda Sedang belajar apa?");
+        JOptionPane.showMessageDialog(null, "Belajar "+input+" sangat mudah","Message",JOptionPane.INFORMATION_MESSAGE);
+        
+        
+    }
+}
